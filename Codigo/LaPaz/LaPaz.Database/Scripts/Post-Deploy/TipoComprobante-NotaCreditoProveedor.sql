18d1342f9998fcd01edf703ef63946f1a8fafa18
silviaeaguilar <silviaeaguilar@gmail.com>
2015-09-21 18:29:37 -0300

    Nota de Credito Proveedor

---
+++ Codigo/LaPaz/LaPaz.Database/Scripts/Post-Deploy/TipoComprobante-NotaCreditoProveedor.sql
@@ -0,0 +1,29 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+
+
+INSERT INTO [TiposComprobantes]
+           ([Id],
+		   [Nombre]
+           ,[Abreviatura]
+           ,[Descripcion]
+           ,[FechaAlta]
+           )
+     SELECT
+			41
+           ,'NOTA DE CREDITO PROVEEDOR'
+           ,'NTCREDRPRV'
+           ,'NOTA DE CREDITO'
+           ,GETDATE()
+           
+		   WHERE NOT EXISTS (SELECT 1 FROM TiposComprobantes WHERE Id = 41)
